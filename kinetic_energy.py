f24e88e4b7f9e78a50a7a295b3f99462656cdbca
Ahmed Shahrour <as.ahmed.shahrour@gmail.com>
2022-02-03 20:12:20 -0800

    Fix functions

--- kinetic_energy.py
+++ kinetic_energy.py
@@ -6,32 +6,37 @@ class Distance:
         self.unit = unit
         self.value = value
 
+
 class Mass:
     def __init__(self, value, unit):
         self.value = value
         self.unit = unit
 
+
 def calculate_kinetic_energy(mass, distance, time):
+    adjusted_distance = 0
     if distance.unit != 'km':
-        if distance.unit == "ly":  # [ly] stands for light-year (measure of distance in astronomy)
+        # [ly] stands for light-year (measure of distance in astronomy)
+        if distance.unit == "ly":
             # convert from light-year to km unit
-            in_km = distance.value * 9.461e12
-            distance = Distance(in_km, "km")
+            adjusted_distance = distance.value * 9.461e12
         else:
-            print ("unit is Unknown")
+            print("unit is Unknown")
             return
-    speed = distance.value/time # [km per sec]
+
+    adjusted_mass = 0
     if mass.unit != 'kg':
         if mass.unit == "solar-mass":
             # convert from solar mass to kg
-            value = mass.value * 1.98892e30 # [kg]
-            mass = Mass(value, 'kg')
+            adjusted_mass = mass.value * 1.98892e30
         else:
-            print ("unit is Unknown")
+            print("unit is Unknown")
             return
 
-    kinetic_energy = 0.5 * mass.value * speed ** 2
+    speed = adjusted_distance / time  # [km per sec]
+    kinetic_energy = 0.5 * adjusted_mass * speed ** 2
     return kinetic_energy
+
 
 mass = Mass(2, "solar-mass")
 distance = Distance(2, 'ly')
--- save_to_db.py
+++ save_to_db.py
@@ -1,12 +1,16 @@
 # By Kami Bigdely
 # Split temp variable
 
+def get_input(message):
+    return input(message)
+
 def save_into_db(info):
     print("saved into databse")
 
+username = get_input('Please enter your username: ')
+save_into_db(username)
 
-user_input = input('Please enter your username: ')
-save_into_db(user_input)
-user_input = int(input('Please enter your birth year: '))
-age = 2020 - user_input
-print("You are",age, "years old.")
+birth_year = int(get_input('Please enter your birth year: '))
+age = 2020 - birth_year
+
+print("You are", age, "years old.")
